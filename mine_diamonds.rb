cad1ced5fb0c65299771c42c05a2ebd19269aba0
hurik zamith <hurikzamith@gmail.com>
2023-04-15 10:08:43 -0300

    clean code

--- mine_diamonds.rb
+++ mine_diamonds.rb
@@ -18,13 +18,13 @@
 ############################################################################################################
 
 def mine_diamonds(expression)
-    diamonds_quantity = 0
-    expression.delete!(".")
-    while expression.include?("<>") do
-        diamonds_quantity += expression.scan(/<>/).count
-        expression.gsub!("<>","")
-    end
-    puts "=" * 50
-    puts "> You have mined #{diamonds_quantity} Diamonds! Congratulations! :)"
-    puts "=" * 50
+  diamonds_quantity = 0
+  expression.delete!('.')
+  while expression.include?('<>')
+    diamonds_quantity += expression.scan(/<>/).count
+    expression.gsub!('<>', '')
+  end
+  puts '=' * 50
+  puts "> You have mined #{diamonds_quantity} Diamonds! Congratulations! :)"
+  puts '=' * 50
 end
--- mine_diamonds_test.rb
+++ mine_diamonds_test.rb
@@ -5,7 +5,7 @@ class MineDiamondsTest < Minitest::Test
 
   def test_no_input
     assert_output("==================================================\n> You have mined 0 Diamonds! Congratulations! :)\n==================================================\n") do
-      mine_diamonds("")
+      mine_diamonds('')
     end
   end
 
@@ -23,25 +23,25 @@ class MineDiamondsTest < Minitest::Test
 
   def test_input_with_one_diamond
     assert_output("==================================================\n> You have mined 1 Diamonds! Congratulations! :)\n==================================================\n") do
-      mine_diamonds("<>")
+      mine_diamonds('<>')
     end
   end
 
   def test_input_with_diamond_with_sand
     assert_output("==================================================\n> You have mined 2 Diamonds! Congratulations! :)\n==================================================\n") do
-      mine_diamonds("<.>.<.>")
+      mine_diamonds('<.>.<.>')
     end
   end
 
   def test_input_with_elaborate_expression
     assert_output("==================================================\n> You have mined 3 Diamonds! Congratulations! :)\n==================================================\n") do
-      mine_diamonds("<.<.>.>.<.>.")
+      mine_diamonds('<.<.>.>.<.>.')
     end
   end
 
   def test_input_with_full_expression_given
     assert_output("==================================================\n> You have mined 11 Diamonds! Congratulations! :)\n==================================================\n") do
-      mine_diamonds("<<.<<..>><>><.>.>.<<.>.<.>>>><>><>>")
+      mine_diamonds('<<.<<..>><>><.>.>.<<.>.<.>>>><>><>>')
     end
   end
 end
